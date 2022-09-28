123410abec891cd73c7ee0296ca2030531d2e1ef
Ty Stucki <tystucki@Tys-MacBook-Pro.local>
2022-09-28 16:17:21 -0700

    changed some folders around

---
+++ client/investing.js
@@ -0,0 +1,27 @@
+
+// const baseURL = 'http://localhost:5020'
+
+// const form = document.querySelector('form')
+
+// function investmentCalc(){
+
+let starting = document.querySelector('#starting')
+let contribution = document.querySelector('#contribution')
+let current = document.querySelector('#currentAge')
+let retirement = document.querySelector('#retirementAge')
+let rate = document.querySelector('#returnRate')
+
+let years = (retirement - current)
+console.log(years)
+
+
+// function investmentCalc() {
+//     let years = (retirement - current)
+//     console.log(years)
+// }
+// console.log(years)
+// console.log('hello world')
+
+// redirect input
+//write the math
+//add event listner for the calculate button
